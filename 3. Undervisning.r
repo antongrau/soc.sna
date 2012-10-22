ccc95cd7c382e8bdf49f2d7b81dcca7a2228bd68
Anton Grau Larsen <alar@soc.ku.dk>
2012-10-22 09:51:04 +0200

    Undervisning nu med graphs!

--- 3. Undervisning.r
+++ 3. Undervisning.r
@@ -11,17 +11,18 @@ library(igraph)
 download.file("https://raw.github.com/antongrau/soc.sna/master/Organisation_BIQ_top.csv", destfile="Organisation_BIQ_top.csv")
 download.file("https://raw.github.com/antongrau/soc.sna/master/Relation_BIQ_top.csv", destfile="Relation_BIQ_top.csv")
 download.file("https://raw.github.com/antongrau/soc.sna/master/delbeskrivelse.r", destfile="delbeskrivelse.r")
+download.file("https://raw.github.com/antongrau/soc.sna/master/gplot2.R", destfile="gplot2.r")
 
 # Mac/Linux
 # Hvis du har en gammel mac så må du hente det ned manuelt og ligge filerne direkte i dit working directory
 download.file("https://raw.github.com/antongrau/soc.sna/master/Organisation_BIQ_top.csv", destfile="Organisation_BIQ_top.csv", method="curl")
 download.file("http://raw.github.com/antongrau/soc.sna/master/Relation_BIQ_top.csv", destfile="Relation_BIQ_top.csv", method="curl")
 download.file("https://raw.github.com/antongrau/soc.sna/master/delbeskrivelse.r", destfile="delbeskrivelse.r", method="curl")
+download.file("https://raw.github.com/antongrau/soc.sna/master/gplot2.R", destfile="gplot2.r", method="curl")
 
 # Indlæs data
 rel           <- read.csv("Relation_BIQ_top.csv", sep="|", encoding="UTF-8", strip.white=TRUE) 
 virk          <- read.csv(file="Organisation_BIQ_top.csv", sep="|", fileEncoding="UTF-8", dec = ",")
-source("delbeskrivelse.r", encoding="UTF-8")
 
 edges         <- data.frame(as.character(rel$NAVN), as.character(rel$ORG_NAVN))
 edge.table    <- table(edges)
@@ -32,12 +33,22 @@ diag(adj.virk) <- 0
 net.virk      <- graph.adjacency(adj.virk, mode="undirected", weighted=TRUE)
 
 ##################################################
+############### Plot #############################
+source("gplot2.r")
+layout <- layout.kamada.kawai(net.virk)
+gplot(net.virk, vertex.coord=layout, text.alpha=0)
+
+##################################################
 ############### Degrees ##########################
 
 degree.virk    <- sort(degree(net.virk), decreasing=TRUE)
 plot(degree.virk)
 table(degree.virk)
 as.matrix(head(degree.virk, 20))
+
+deg <- degree(net.virk)
+n <- gplot(net.virk, vertex.coord=layout, vertex.fill=deg ,text.alpha=0, vertex.size=4, edge.alpha=0.1)
+n + scale_fill_gradient2(high = "red")
 
 totaldegree     <- sum(degree.virk)
 totaldegree
@@ -66,6 +77,11 @@ as.matrix(head(sort(n3, decreasing=TRUE), 20))
 
 plot(sort(n3-n2-n1))
 
+
+### Et lille plot
+neighbours <- neighborhood.size(net.virk, 2)
+n <- gplot(net.virk, vertex.coord=layout, vertex.fill=neighbours ,text.alpha=0, vertex.size=4, edge.alpha=0.1)
+n + scale_fill_gradient2(high = "blue")
 
 #######################################################
 ################# Density #############################
@@ -107,6 +123,8 @@ summary(ansatte)
 egenkapital <- cut(virk$EGENKAPITAL.10, quantile(virk$EGENKAPITAL.10, na.rm=TRUE))
 levels(egenkapital) <- c("1. kvartil: Mindst", "2. kvartil", "3. kvartil", "4. kvartil: Størst")
 summary(egenkapital)
+
+source("delbeskrivelse.r", encoding="UTF-8")
 
 del.beskrivelse(net.virk, omsætning)
 del.beskrivelse(net.virk, virk$BØRSEN.BRANCHE)
