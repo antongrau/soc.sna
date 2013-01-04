783e1132e345432dd4e3ac6096a12da6eef9cbb3
Anton Grau Larsen <alar@soc.ku.dk>
2013-01-04 11:35:02 +0100

    update

--- identificer_samlet.R
+++ identificer_samlet.R
@@ -23,33 +23,34 @@ data$Navn       <- nu
 
 
 rel            <- read.csv("Data_virksomheder_rel.csv", sep="|", fileEncoding="UTF-8")
-
 rel.n          <- as.character(rel$NAVN)[rel$NAVN %in% nu]
 rel.id         <- as.character(rel$BIQ_PERSON_ID)[rel$NAVN %in% nu]
-fest <- unique(data.frame(rel.n, rel.id))
-tb <- table(fest$rel.n)
-fest <- fest[order(fest[,1]),]
-eliminate <- c(1154180, 270261, 908748, 282522, 1655058, 468144, 468165, 989328, 513857, 1605072, 1881695, 919971, 570550)
-festa <- fest[(fest[,2] %in% eliminate)==FALSE ,]
-festa[,2] <- as.numeric(as.character(festa[,2]))
+fest           <- unique(data.frame(rel.n, rel.id))
+tb             <- table(fest$rel.n)
+fest           <- fest[order(fest[,1]),]
+eliminate      <- c(1154180, 270261, 908748, 282522, 1655058, 468144, 468165, 989328, 513857, 1605072, 1881695, 919971, 570550)
+festa          <- fest[(fest[,2] %in% eliminate)==FALSE ,]
+festa[,2]      <- as.numeric(as.character(festa[,2]))
 festa[,2][festa[,1] %in% c("Jørgen Jensen", "Lars Jensen", "Niels Fog")]   <- NA
-festa <- festa[order(festa[,1]),]
-data$BIQ_ID <- festa[,2]
+festa          <- festa[order(festa[,1]),]
+data           <- data[order(data$Navn),]
+data$BIQ_ID    <- festa[,2]
+
 
 ##############################
 # find advokaterne
 
-advokat <- read.csv("data_advokater.csv", sep="|", fileEncoding="UTF-8")
+advokat        <- read.csv("data_advokater.csv", sep="|", fileEncoding="UTF-8")
 advokat$X[is.na(advokat$X)] <- 0
-advokat <- advokat[advokat$BIQ_ID != 0,] # Her fjerner vi alle advokater uden bestyrelsesposter - for de har ingen BIG_ID
-advokat_biq <- advokat[advokat$X != 0,]
-advokat <- advokat[advokat$X==0,]  # Her fjerner vi alle bestyrelsesmedlemmer der ikke er advokater - de har en anden X værdi end 0   
-advokat <- advokat[duplicated(advokat$BIQ_ID)==FALSE,] # Her fjerner vi nogle advokater der af en eller anden årsag opstod flere gange. Det er 2.
+advokat        <- advokat[advokat$BIQ_ID != 0,] # Her fjerner vi alle advokater uden bestyrelsesposter - for de har ingen BIG_ID
+advokat_biq    <- advokat[advokat$X != 0,]
+advokat        <- advokat[advokat$X==0,]  # Her fjerner vi alle bestyrelsesmedlemmer der ikke er advokater - de har en anden X værdi end 0   
+advokat        <- advokat[duplicated(advokat$BIQ_ID)==FALSE,] # Her fjerner vi nogle advokater der af en eller anden årsag opstod flere gange. Det er 2.
 advokat_BIQ_navn <- advokat_biq$Navn[advokat_biq$BIQ_ID %in% advokat$BIQ_ID]
 
-advokat.ind <- as.factor(n %in% advokat_BIQ_navn)
+advokat.ind    <- as.factor(data$Navn %in% advokat_BIQ_navn)
 levels(advokat.ind) <- c("Ikke advokat", "Advokat")
-data$advokat <- advokat.ind
+data$advokat   <- advokat.ind
 
 summary(advokat.ind)
 n[advokat.ind == "Advokat"]
@@ -82,7 +83,7 @@ find.køn <- function(navne, names.gender=names.gender){
   return(kategori)
 }
 
-køn <- find.køn(nu, gender.mat)
+køn <- find.køn(data$Navn, gender.mat)
 data$køn <- køn
 
 #################################
@@ -102,8 +103,6 @@ data$kommissionspost <- kommissionspost
 
 ###############################
 ### Gem
-
 data <- data[, -1]
-
 write.table(data, "Data_inderkreds_individer.csv", sep="|", fileEncoding="UTF-8", row.names = FALSE)
 
